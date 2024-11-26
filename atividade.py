b60acd22beba99e4073d4e5c2253558d0da79c08
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2024-11-26 12:48:54 -0300

    caminho absoluto do arquivo para que seja salvo na pasta correta

--- atividade.py
+++ atividade.py
@@ -3,5 +3,9 @@ import urllib
 import os
 import json
 
+diretorio = "C:/Users/bruvieira/Desktop/Nova_pasta/Atividade_Github"
+nome_arquivo = "atividade.json" 
+caminho_completo = os.path.join(diretorio, nome_arquivo)
+
 
 

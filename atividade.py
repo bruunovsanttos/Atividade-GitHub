e3f19b59f278e58d197ecec843cb01ec62b9f59e
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2024-11-27 12:20:18 -0300

    (feat)função: adicionando função de abrir json

--- atividade.py
+++ atividade.py
@@ -4,8 +4,18 @@ import os
 import json
 
 diretorio = "C:/Users/bruvieira/Desktop/Nova_pasta/Atividade_Github"
-nome_arquivo = "atividade.json" 
+nome_arquivo = "atividade.json"
 caminho_completo = os.path.join(diretorio, nome_arquivo)
+
+url = "https://api.github.com/users/<username>/events"
+
+def salva_json():
+    if os.path.exists(caminho_completo):
+        with open("atividade.json", "r", encoding="utf-8") as arquivo:
+            atividade = json.load(arquivo)
+
+
+
 
 
 

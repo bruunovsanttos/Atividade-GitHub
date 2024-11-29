45e4179d7d842406604863cba7735ef7e6b9e094
bruunovsanttos <bruno-vieira-santos@hotmail.com>
2024-11-29 12:52:59 -0300

    (fix)função: ajustando função salvajsonpara atuar corretamente

--- atividade.py
+++ atividade.py
@@ -4,7 +4,7 @@ import json
 import requests
 
 
-diretorio = "C:/Users/bruvieira/Desktop/Nova_pasta/Atividade_Github"
+diretorio = os.getcwd()
 nome_arquivo = "atividade.json"
 caminho_completo = os.path.join(diretorio, nome_arquivo)
 
@@ -16,12 +16,16 @@ def salva_json(usuario):
     try:
 
         resposta = requests.get(url)
+        print(f"Status Code: {resposta.status_code}")
+        print(f"Conteúdo da Resposta: {resposta.text[:200]}")
 
         if resposta.status_code == 200:
-            if os.path.exists(caminho_completo):
-                with open(caminho_completo, "w", encoding="utf-8") as arquivo:
-                    json.dump(resposta.json(), arquivo, ensure_ascii=False, indent=4)
-                    print(f"A atividade do usuario {usuario} foi salva com sucesso no arquivo {nome_arquivo}")
+            atividades = resposta.json()
+            if atividades:
+                if os.path.exists(caminho_completo):
+                    with open(caminho_completo, "w", encoding="utf-8") as arquivo:
+                        json.dump(resposta.json(), arquivo, ensure_ascii=False, indent=4)
+                        print(f"A atividade do usuario {usuario} foi salva com sucesso no arquivo {nome_arquivo}")
             else:
                 print(f"Não foi possivel encontrar atividade do usuário {usuario}")
 
@@ -34,18 +38,47 @@ def salva_json(usuario):
         print(f"Erro de conexão com a API: {e}")
 
 
+
+#aqui esta como funciona corretamente verificar onde esta o erro para atender o codigo de uma forma satisfatoria.
+
+        #resposta = requests.get(url)
+
+        # Exibir a resposta da API para depuração
+        #print(f"Status Code: {resposta.status_code}")
+        #print(f"Conteúdo Completo da Resposta: {resposta.text}")  # Exibe o conteúdo completo da resposta da API
+
+        #if resposta.status_code == 200:
+            # Verifica se há conteúdo na resposta
+         #   atividades = resposta.json()
+          #  if atividades:
+           #     with open(caminho_completo, "w", encoding="utf-8") as arquivo:
+            #        json.dump(atividades, arquivo, ensure_ascii=False, indent=4)
+             #       print(f"A atividade do usuário {usuario} foi salva com sucesso no arquivo {nome_arquivo}")
+            #else:
+             #   print(f"O usuário {usuario} não tem atividades recentes.")
+        #elif resposta.status_code == 404:
+         #   print(f"Erro: Usuário {usuario} não encontrado no GitHub.")
+        #else:
+         #   print(f"Erro ao buscar a atividade do usuário {usuario}. Status code: {resposta.status_code}")
+
+    #except requests.exceptions.RequestException as e:
+     #   print(f"Erro de conexão com a API: {e}")
+
+
 def exibir_atividade():
     if os.path.exists(caminho_completo):
         with open(caminho_completo, "r", encoding="utf-8") as arquivo:
             atividades = json.load(arquivo)
 
             if atividades:
-                for atividade in atividades:
+                print("\n Atividades Recentes: ")
+                for atividade in atividades[:5]:
                     tipo = atividade.get('type')
                     repo = atividade['repo']['name']
 
                     if tipo == 'PushEvent':
-                        print(f"Commits feitos no {repo}")
+                        commits = atividade.get('payload', {}).get('commits', [])
+                        print(f"Enviados {len(commits)} Commits feitos no {repo}")
 
 
 def main():
